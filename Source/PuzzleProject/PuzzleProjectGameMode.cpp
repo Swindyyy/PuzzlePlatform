9669180ea02ea5c26c491f48ec24bf59b86063aa
Swindyyy <bs309@kent.ac.uk>
2018-08-25 23:06:04 +0100

    Inital commit

---
+++ Source/PuzzleProject/PuzzleProjectGameMode.cpp
@@ -0,0 +1,15 @@
+// Copyright 1998-2018 Epic Games, Inc. All Rights Reserved.
+
+#include "PuzzleProjectGameMode.h"
+#include "PuzzleProjectCharacter.h"
+#include "UObject/ConstructorHelpers.h"
+
+APuzzleProjectGameMode::APuzzleProjectGameMode()
+{
+	// set default pawn class to our Blueprinted character
+	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnBPClass(TEXT("/Game/ThirdPersonCPP/Blueprints/ThirdPersonCharacter"));
+	if (PlayerPawnBPClass.Class != NULL)
+	{
+		DefaultPawnClass = PlayerPawnBPClass.Class;
+	}
+}
